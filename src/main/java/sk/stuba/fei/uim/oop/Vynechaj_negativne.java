f533984027cea7e062cc6e16be6afff1a1e7f387
Jakub <xchrappaj@stuba.sk>
2022-03-02 13:43:18 +0100

    cviko3

---
+++ src/main/java/sk/stuba/fei/uim/oop/Vynechaj_negativne.java
@@ -0,0 +1,19 @@
+package sk.stuba.fei.uim.oop;
+
+public class Vynechaj_negativne {
+    public static void main(String[] args) {
+
+        int n = args.length;
+
+        for (int i = 0; i < n ; i++){
+
+            int x = Integer.parseInt(args[i]);
+
+            System.out.println("args" + i + " : " + x);
+
+        }
+
+
+
+    }
+}
