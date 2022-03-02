512b27ca1bac88b84a0a677f037ad7b86514d94a
Jakub <xchrappaj@stuba.sk>
2022-03-02 14:21:13 +0100

    cviko3

---
+++ src/main/java/sk/stuba/fei/uim/oop/Pole_poly_pol_poly.java
@@ -0,0 +1,14 @@
+package sk.stuba.fei.uim.oop;
+
+import sk.stuba.fei.uim.oop.utility.ZKlavesnice;
+
+public class Pole_poly_pol_poly {
+    public static void main(String[] args) {
+
+        int[][] pole = new int [][]
+                {{1,2,3},{7,9,8},{12,32}};
+
+        System.out.println(pole[1][2]);
+
+    }
+}
