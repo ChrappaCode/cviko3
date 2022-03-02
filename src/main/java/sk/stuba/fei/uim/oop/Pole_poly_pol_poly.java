0293938c1b94798bf4af2079ef74de2ddb03d993
Jakub <xchrappaj@stuba.sk>
2022-03-02 14:33:14 +0100

    cviko3

--- src/main/java/sk/stuba/fei/uim/oop/Pole_poly_pol_poly.java
+++ src/main/java/sk/stuba/fei/uim/oop/Pole_poly_pol_poly.java
@@ -5,10 +5,33 @@ import sk.stuba.fei.uim.oop.utility.ZKlavesnice;
 public class Pole_poly_pol_poly {
     public static void main(String[] args) {
 
-        int[][] pole = new int [][]
+        /*
+        int[][] pol = new int [][]
                 {{1,2,3},{7,9,8},{12,32}};
 
-        System.out.println(pole[1][2]);
+        System.out.println(pol[1][2]);
+        */
+
+        //takze podme sa pozriet na realne priklady
+
+        int pocet = ZKlavesnice.readInt("Zadaj pocet prvkov pola");
+
+        System.out.println("pocet prvkov: " + pocet);
+
+        int[] pole = new int[pocet];
+
+        for(int i = 0; i < pole.length; i++){
+            pole[i] = ZKlavesnice.readInt("Zadaj prvok pole["+i+"]");
+
+        }
+
+        System.out.println("Vypis pole");
+
+        for(int y = 0; y < pole.length; y++){
+            System.out.print(pole[y]+", ");
+        }
+        System.out.println();
+        System.out.println("obsah premennej pole je "+pole);
 
     }
 }
