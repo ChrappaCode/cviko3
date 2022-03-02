23e2506adce9dae8e4db93d8287240296a725373
Jakub <xchrappaj@stuba.sk>
2022-03-02 13:32:01 +0100

    Trojuholnik

--- src/main/java/sk/stuba/fei/uim/oop/Trojuholnik.java
+++ src/main/java/sk/stuba/fei/uim/oop/Trojuholnik.java
@@ -8,9 +8,9 @@ public class Trojuholnik {
 
         int vyska = ZKlavesnice.readInt("Zadaj pocet riadkov ");
 
-        for(int riadok = 1; riadok<=vyska;riadok++) {
+        for(int riadok = 1; riadok <= vyska;riadok++) {
             for (int stlpec = 1; stlpec <= riadok; stlpec++) {
-                System.out.println("*");
+                System.out.print("*");
             }
             System.out.println();
         }
